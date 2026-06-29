aa0f6c221658301ebe11949b1d642e1b2c5830a8
Ebrahim Aliyou Wudu <ebrahimaliyou@gmail.com>
2026-06-29 22:42:25 +0300

    Fix: conch encoder architectural mismatch

--- get_processor.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-url = "https://raw.githubusercontent.com/huggingface/transformers/v4.40.0/src/transformers/models/llava/processing_llava.py"
-urllib.request.urlretrieve(url, "processing_llava.py")
